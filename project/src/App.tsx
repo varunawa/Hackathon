365d3048569fda0b8b2c29d36a2488284265c9b2
varunawa <dnvdgcrisostomo@gmail.com>
2024-10-05 13:26:42 +1000

    added data parsing

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css'
 import ThreeScene from './components/testScene'
+import BeaconParser from './components/BeaconParser';
 
 function App() {
   return (
     <>
         <ThreeScene></ThreeScene>
+        <BeaconParser />
+        <ThreeScene />
     </>
   )
 }
