723db77ace6d56f27058d6c900501b7dbdfe78f5
Katherine-Kong <160790348+KathyK0513@users.noreply.github.com>
2024-10-05 15:13:54 +1000

    basic interface

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -36,7 +36,7 @@ function App() {
   return (
 
     <div className="App">
-      <h1>Parsed Beacon Messages</h1>
+      <h1>Beacon 3D Visualisation</h1>
 
       {error && <p style={{ color: 'red' }}>{error}</p>}
       
